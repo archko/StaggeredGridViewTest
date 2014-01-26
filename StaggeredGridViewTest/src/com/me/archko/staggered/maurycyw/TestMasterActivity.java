a3f8f8844978d8f40233bbf3424af70981f137c5
archko <wrf14174>
2014-01-26 21:18:05 +0800

    add master gridview test,selectable column

--- StaggeredGridViewTest/src/com/me/archko/staggered/maurycyw/TestMasterActivity.java
+++ StaggeredGridViewTest/src/com/me/archko/staggered/maurycyw/TestMasterActivity.java
@@ -1,14 +1,14 @@
 package com.me.archko.staggered.maurycyw;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import com.mani.staggeredview.demo.model.FlickrGetImagesResponse;
 import com.mani.staggeredview.demo.model.FlickrImage;
 import com.mani.staggeredview.demo.model.FlickrResponsePhotos;
 import com.me.archko.staggered.BaseFlickrPictureActivity;
-import com.me.archko.staggered.PictureViewerActivity;
 import com.me.archko.staggered.R;
 import com.me.archko.staggered.utils.Util;
 import com.origamilabs.library.views2.StaggeredGridView;
@@ -70,5 +70,26 @@ public class TestMasterActivity extends BaseFlickrPictureActivity {
         mAdapter.setDatas(list);
 
         mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, Menu.FIRST, 0, "One column");
+        menu.add(0, Menu.FIRST+1, 1, "Two column");
+        menu.add(0, Menu.FIRST+2, 2, "Three column");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if (id==Menu.FIRST) {
+            mStaggeredGridView.setColumnCount(1);
+        } else if (id==Menu.FIRST+1) {
+            mStaggeredGridView.setColumnCount(2);
+        } else if (id==Menu.FIRST+2) {
+            mStaggeredGridView.setColumnCount(3);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
